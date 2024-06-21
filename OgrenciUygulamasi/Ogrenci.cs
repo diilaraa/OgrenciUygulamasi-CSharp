7a4fffe56d8c9276c24dfecd8c1e59679e789978
Dilara Top <dilaratop02@gmail.com>
2024-06-21 16:25:48 +0300

    Proje dosyası ekle.

---
+++ OgrenciUygulamasi/Ogrenci.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OgrenciUygulamasi
+{
+    public class Ogrenci
+    {
+        private int ogrenciId;
+        private string isim;
+        private string soyisim;
+        private int vize;
+        private int kisaSinav;
+        private int final;
+        private string okulIsmi;
+
+        public void ogrenciBilgileriGoster()
+        {
+            Console.WriteLine("Öğrenci Numarası:" + ogrenciId);
+            Console.WriteLine("Öğrenci İsmi:" + isim);
+            Console.WriteLine("Öğrenci Soyismi:" + soyisim);
+            Console.WriteLine("Öğrenci Vize Notu:" + vize);
+            Console.WriteLine("Öğrenci Kısa Sınav Notu:" + kisaSinav);
+            Console.WriteLine("Öğrenci Final Notu:" + final);
+            Console.WriteLine("Öğrenci Üniversite İsmi:" + okulIsmi);
+        }
+
+    }
+}
---
+++ OgrenciUygulamasi/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+öğrencinin öğrenciNo, isim, soyisim, vize, kısasınav, final ve okulismi adında değişkenler olacak ve erişim belirleyicileri private durumda olacak.
+ogrenciBilgiGoster(),ogrenciOrtalamasiBul() ve okulGetir() adında metotlar bulunacak.
+Ögrencinin seçimlerine göre islemler yapilacak: 
+1-Öğrenbci Bilgilerini gösterr
+2-Öğrenci Ortalşamasını Göster
+3-Öğrencinin okulunu getir
+4-Çıkışi Yap
+*/
+namespace OgrenciUygulamasi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
