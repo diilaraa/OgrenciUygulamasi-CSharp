cddc541a62a0b8a4bca022ecd0e8d99523d78fe8
Dilara Top <dilaratop02@gmail.com>
2024-06-21 16:50:50 +0300

    tamamlandı

--- OgrenciUygulamasi/Ogrenci.cs
+++ OgrenciUygulamasi/Ogrenci.cs
@@ -16,6 +16,16 @@ namespace OgrenciUygulamasi
         private int final;
         private string okulIsmi;
 
+        public Ogrenci(int _ogrenciID, string _isim, string _soyisim, int _vize, int _kisaSinav, int _final, string _okulIsmi) //Parametere olduğu anlaşılsın diye _ koydum
+        {
+            ogrenciId = _ogrenciID;
+            isim = _isim;
+            soyisim = _soyisim;
+            vize = _vize;
+            kisaSinav = _kisaSinav; 
+            final = _final; 
+            okulIsmi = _okulIsmi;
+        }
         public void ogrenciBilgileriGoster()
         {
             Console.WriteLine("Öğrenci Numarası:" + ogrenciId);
@@ -26,6 +36,15 @@ namespace OgrenciUygulamasi
             Console.WriteLine("Öğrenci Final Notu:" + final);
             Console.WriteLine("Öğrenci Üniversite İsmi:" + okulIsmi);
         }
+        public double OgrenciNotu() //not virgüllüde olabileceğinden double tanımlandı
+        {
+            double ortalama = vize*0.35 + kisaSinav*0.15 + final*0.5;
+            return ortalama;
+        }
 
+        public void okulGetir()
+        {
+            Console.WriteLine("Ogrencinin okuduğu Üniversite:" + okulIsmi);
+        }
     }
 }
--- OgrenciUygulamasi/Program.cs
+++ OgrenciUygulamasi/Program.cs
@@ -7,10 +7,10 @@ using System.Threading.Tasks;
 öğrencinin öğrenciNo, isim, soyisim, vize, kısasınav, final ve okulismi adında değişkenler olacak ve erişim belirleyicileri private durumda olacak.
 ogrenciBilgiGoster(),ogrenciOrtalamasiBul() ve okulGetir() adında metotlar bulunacak.
 Ögrencinin seçimlerine göre islemler yapilacak: 
-1-Öğrenbci Bilgilerini gösterr
+1-Öğrenci Bilgilerini gösterr
 2-Öğrenci Ortalşamasını Göster
 3-Öğrencinin okulunu getir
-4-Çıkışi Yap
+4-Çıkış Yap
 */
 namespace OgrenciUygulamasi
 {
@@ -18,6 +18,47 @@ namespace OgrenciUygulamasi
     {
         static void Main(string[] args)
         {
+            bool kontrol = true;
+            Ogrenci ogrenci1 = new Ogrenci(2005416458, "Elisa", "Badeci", 75, 63, 80, "Kaynarca Üniversitesi") ;
+            Console.WriteLine("Öğrenci Bilgi Sistemine Hoşgeldiniz");
+            Console.WriteLine("Gerçekleştirmek istediğiniz işlemi seçiniz");
+            islemler();
+
+            while (kontrol)
+            {
+                string secim = Console.ReadLine();
+                switch(secim)
+                {
+                    case "1":
+                        ogrenci1.ogrenciBilgileriGoster();
+                        break;
+
+                    case "2":
+                        double ogrenciOrtalama = ogrenci1.OgrenciNotu();
+                        Console.WriteLine("Öğrencinin Ortalaması: " + ogrenciOrtalama);
+                        break;
+
+                    case "3":
+                        ogrenci1.okulGetir();
+                        break;
+
+                    case "4":
+                        kontrol = false;
+                        break;
+
+
+                }
+
+            }
+            
+        }
+
+        static void islemler()
+        {
+            Console.WriteLine("1 - Öğrenci Bilgilerini Göster");
+            Console.WriteLine("2 - Öğrenci Ortalamasını Göster");
+            Console.WriteLine("3 - Öğrencinin okulunu getir");
+            Console.WriteLine("4 - Çıkış Yap");
         }
     }
 }
